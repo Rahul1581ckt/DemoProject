bc2d78a0dfd267d678eb3630007fd3808fc92476
RA349382 <RA349382@L-156206976.wipro.com>
2019-12-14 18:27:51 +0530

    Commit 1

---
+++ MavenWebApp/src/main/resources/SimpleServlet.java
@@ -0,0 +1,14 @@
+
+
+@WebServlet("/hello")
+public class SimpleServlet extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+         throws ServletException, IOException {
+      resp.setContentType("text/plain");
+      resp.getWriter().write("Hello World! Maven Web Project Example.");
+   }
+}
+
+}
